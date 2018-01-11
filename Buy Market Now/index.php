12a7ed02a2162212dc022d3b9f857dc24381dbde
Nannim <Nannim@users.noreply.github.com>
2018-01-12 00:27:19 +0100

    css for bmn

--- Buy Market Now/index.php
+++ Buy Market Now/index.php
@@ -1,58 +1,29 @@
 <head>
 <title>Welcome - Buy Market Now</title>
+<link rel="stylesheet" href="buymarketnow.css" type="text/css"/>
 <link rel="shortcut icon" href="buymarketnow.jpg" type="image/jpg"/>
 </head>
 <body>
-    <div class="menu-slab">
       <table border="0" width="100%">
         <tr>
-        <td width="20%"><a href="#home">
-          <img src="buymarketnow.jpg" type="image/jpg" style="width:80px;"/></a>
+        <td width="20%">
+          <a href="http://www.buymarketnow.com/">
+          <img src="buymarketnow.jpg" type="image/jpg" style="width:80px;"/>
+          </a>
         </td>
-        <td width="12%"><a href="#home"><button>HOME</button></a></td>
-        <td width="12%"><a href="#market"><button>MARKET</button></a></td>
-        <td width="12%"><a href="#shopper"><button>SHOPPER</button></a></td>
-        <td width="12%"><a href="#cart"><button>CART</button></a></td>
-        <td width="12%"><a href="#delivery"><button>DELIVERY</button></a></td>
-        <td width="12%"><a href="#help"><button>HELP</button></a></td>
+        <td width="12%"><button><a href="http://www.buymarketnow.com/">HOME</a></button></td>
+        <td width="12%"><button><a href="http://www.buymarketnow.com/market/">MARKET</a></button></td>
+        <td width="12%"><button><a href="http://www.buymarketnow.com/shopper/">SHOPPER</a></button></td>
+        <td width="12%"><button><a href="http://www.buymarketnow.com/cart/">CART</a></button></td>
+        <td width="12%"><button><a href="http://www.buymarketnow.com/delivery/">DELIVERY</a></button></td>
+        </tr>
+        <tr>
+          <td></td><td></td><td></td><td></td>
+          <td><button><a href="http://www.buymarketnow.com/login/">login</a></button></td>
+          <td><button><a href="http://www.buymarketnow.com/signup/">signup</a></button></td>
         </tr>
       </table>
-    </div>
-    <style>
-        *
-        {
-          font-family:Calibri;
-          text-align:center;
-          text-decoration: none;
-        }
-        button
-        {
-          padding:10px 10px 10px 10px;
-          background:url("buy.png");
-          border:1px;
-        }
-        .footer{text-align:center;bottom:0;}
-        hr
-        {
-          width:50%;
-          opacity:10%;
-          margin-top:50px;
-        }
-        img
-        {
-          text-align:center;
-          width:800px;
-        }
-        table
-        {
-          position:relative;
-          text-align:center;
-          font-size:12px;
-        }
-    </style>
-
-        <div class="">
-        <hr style="width:90%;opacity:0.2;">
+        <hr style="width:70%;opacity:0.2;">
         <video autoplay controls repeat style="width:800px;height:400px;">
             <source src="buymarketnow.mp4" type="video/mp4"/>
         </video>
@@ -60,17 +31,31 @@
         <hr style="width:90%;opacity:0.2;">
         <br>
         <br>
-        </div>
 </body>
         <hr style="width:90%;opacity:0.2;">
 <div>
   <table border="0" style="width:100%;">
-  <tr><td><a href="#">Mail</a></td><td><a href="#">Partners</a></td><td><a href="#">Mobile</a></td><td><a href="#">Extensions</a></td></tr>
-  <tr><td><a href="#">Feedback</a></td><td><a href="#">Support</a></td><td><a href="#">Developer</a></td><td><a href="#">Open Source</a></td></tr>
-  <tr><td><a href="#">Connect</a></td><td><a href="#">Help</a></td><td><a href="#">Tools</a></td><td><a href="#">EULA</a></td></tr>
+    <th width="25%"></th><th width="25%"></th><th width="25%"></th><th width="25%"></th>
+  <tr><td><a href="http://www.buymarketnow.com/mail/">Mail</a></td>
+    <td><a href="http://www.buymarketnow.com/partners/">Partners</a></td>
+    <td><a href="http://www.buymarketnow.com/mobile/">Mobile</a></td>
+    <td><a href="http://www.buymarketnow.com/extensions/">Extensions</a></td>
+  </tr>
+  <tr><td><a href="http://www.buymarketnow.com/feedback/">Feedback</a></td>
+    <td><a href="http://www.buymarketnow.com/support/">Support</a></td>
+    <td><a href="http://www.buymarketnow.com/developer/">Developer</a></td>
+    <td><a href="http://www.buymarketnow.com/opensource/">Open Source</a></td>
+  </tr>
+  <tr>
+    <td><a href="http://www.buymarketnow.com/connect/">Connect</a></td>
+    <td><a href="http://www.buymarketnow.com/help/">Help</a></td
+      ><td><a href="http://www.buymarketnow.com/tools/">Tools</a></td>
+      <td><a href="http://www.buymarketnow.com/EULA/">EULA</a></td></tr>
     <tr>
-      <td><a href="#">emeraldarchies@gmail.com</a></td><td><a href="#">Copyright &copy;</a></td>
-      <td><a href="#">Buy Market Now 2017</a></td><td><a href="#">Emerald Archies LTD</a></td>
+      <td><a href="http://www.buymarketnow.com/mail/">buymarketnow@emeraldarchies.com</a></td>
+      <td><a href="http://www.buymarketnow.com/EULA/">Copyright &copy;</a></td>
+      <td><a href="http://www.buymarketnow.com/">Buy Market Now 2017</a></td>
+      <td><a href="http://www.emeraldarchiesltd.com/">Emerald Archies LTD</a></td>
     </tr>
 </table>
 </div>
